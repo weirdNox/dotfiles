1e14d6b5957f63e3b7f8c0de7d8adeb2854b6278
Gonçalo Santos <goncalossantos98@gmail.com>
2020-07-14 01:41:37 +0100

    [shell] fix mime install.sh

--- other/mime/install.sh
+++ other/mime/install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail; shopt -s nullglob
 cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 
-mkdir -p ~/.config ~/.local/share/mime
+mkdir -p ~/.config ~/.local/share/mime/packages
 
 cp mimeapps.list ~/.config
 update-mime-database ~/.local/share/mime
