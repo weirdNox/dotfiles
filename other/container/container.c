0d354450cf25457c0c4fb2c203c14a83563c128c
Gonçalo Santos <goncalossantos98@gmail.com>
2020-08-21 17:03:33 +0100

    [container] move defines

--- other/container/container.c
+++ other/container/container.c
@@ -6,14 +6,14 @@
 #define BIND_HOME_PATH
 
 #define SHARE_NETWORK 0
+#define BIND_HOSTNAME host
 
-#define SHARE_DBUS    0
+#define SHARE_DBUS 0
 #define DBUS_SYST_PROXY "--filter",
 #define DBUS_USER_PROXY "--filter",
 
 #define ENVS_TO_KEEP W("DISPLAY"), W("TERM")
 
-#define BIND_HOSTNAME host
 #define BIND_ENV_DEFAULT_PATH "/usr/local/sbin:/usr/local/bin:/usr/bin"
 
 #define ARGS_OVERRIDE_STRING "-"
