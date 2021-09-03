be2a20523ea9a082f8cf739d3f73f0617357a6ae
Gonçalo Santos <goncalossantos98@gmail.com>
2021-09-03 01:19:26 +0100

    [dotfiles] add default value to install script, don't build montengamepad by default

--- other/container/install.sh
+++ other/container/install.sh
@@ -2,4 +2,6 @@
 set -euo pipefail; shopt -s nullglob
 ScriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+: "${XDG_BIN_HOME:=$HOME/.local/bin}"
+mkdir -p "$XDG_BIN_HOME"
 cp -vfas "$ScriptDir/container.h" "$XDG_BIN_HOME"
