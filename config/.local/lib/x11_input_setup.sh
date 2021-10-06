f7d3d8ea04f2642096805fa985f9206313dfad6b
Gonçalo Santos <goncalossantos98@gmail.com>
2021-10-06 15:10:28 +0100

    [shell] improve touchpad configuration

--- config/.local/lib/x11_input_setup.sh
+++ config/.local/lib/x11_input_setup.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -euo pipefail
+
+# NOTE(nox): Keyboard
 setxkbmap -layout us -variant intl -option ctrl:nocaps,shift:both_capslock,compose:rwin
 
-for Device in "SynPS/2 Synaptics TouchPad" "Wacom Intuos PT S 2 Finger"; do
+# NOTE(nox): Touchpads
+readarray -t Touchpads <<< $(xinput list --name-only | grep -i touchpad)
+Touchpads+=("Wacom Intuos PT S 2 Finger")
+
+for Device in "${Touchpads[@]}"; do
     if xinput list-props "$Device" >/dev/null 2>&1; then
         xinput set-prop "$Device" "libinput Tapping Enabled" 0
         xinput set-prop "$Device" "libinput Tapping Drag Enabled" 0
