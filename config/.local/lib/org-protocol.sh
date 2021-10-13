4ad891e0f535861d508081b4d6b931a751d55285
Gonçalo Santos <goncalossantos98@gmail.com>
2021-10-13 12:42:24 +0100

    [emacs] improve server without systemd

--- config/.local/lib/org-protocol.sh
+++ config/.local/lib/org-protocol.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-emacsclient -e "(require 'org-protocol)"
-emacsclient -c -F '((name . "Org Capture")(org-capture-frame . t)(width . 110)(height . 40))' "$@"
+emacsclient  -a "" -e "(require 'org-protocol)"
+emacsclient -ca "" -F '((name . "Org Capture")(org-capture-frame . t)(width . 110)(height . 40))' "$@"
