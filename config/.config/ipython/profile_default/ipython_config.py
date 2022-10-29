919c6bdf3b0f2c74fb5a4195b9c09aeb199b6228
Gonçalo Santos <goncalossantos98@gmail.com>
2022-10-29 16:15:23 +0100

    [ipython] update config

---
+++ config/.config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,4 @@
+c.InteractiveShell.history_length = 5000000
+c.InteractiveShell.history_load_length = c.InteractiveShell.history_length
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.matplotlib = 'auto'
