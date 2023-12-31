38bba28f969598223a1e180c9136416ad2775194
Gonçalo Santos <goncalossantos98@gmail.com>
2023-12-31 23:22:16 +0000

    [pipewire] don't force alsa rate

--- config/.config/wireplumber/main.lua.d/75-alsa-rate.lua
+++
@@ -1,9 +0,0 @@
-alsa_monitor.rules[#alsa_monitor.rules + 1] = {
-    matches = {
-        {{ "node.name", "matches", "alsa_input.*"  }},
-        {{ "node.name", "matches", "alsa_output.*" }},
-    },
-    apply_properties = {
-        ["audio.rate"] = 48000,
-    },
-}
